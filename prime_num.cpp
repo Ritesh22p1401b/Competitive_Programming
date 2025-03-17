8cd8045ceefeb0292e17d25b12abfc0735084114
Ritesh Pandey <ritesh2897pandey@gmail.com>
2025-03-17 17:41:43 +0530

    string

---
+++ prime_num.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int t;cin>>t;
+	while(t--){
+		int n,m;
+		int k=1;
+		cin>>n>>" ">>m;
+		for(int i=0;i<m;i++){
+			if(n%i==0){
+				
+			}
+		}
+	}
+}
---
+++ string.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+	string s="abcd";
+	for(int i=0;i<s.size();i++){
+		cout<<s[i]<<endl;
+	}
+}
